eaf08745bd6d869da8a4da17dd5a468e44a4e272
pagoru <pagoru@gmail.com>
2016-11-02 20:40:03 +0100

    Finiiiishshshshshshshhs

--- vb-project02/Form1.Designer.vb
+++ vb-project02/Form1.Designer.vb
@@ -33,6 +33,15 @@ Partial Class Form1
         Me.button_order = New System.Windows.Forms.Button()
         Me.RadioButton_ASC = New System.Windows.Forms.RadioButton()
         Me.RadioButton_DESC = New System.Windows.Forms.RadioButton()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.button_div = New System.Windows.Forms.Button()
+        Me.lbl_div_numbers = New System.Windows.Forms.Label()
+        Me.TextBox_Month = New System.Windows.Forms.TextBox()
+        Me.lbl_Month = New System.Windows.Forms.Label()
+        Me.lbl_year = New System.Windows.Forms.Label()
+        Me.TextBox_Year = New System.Windows.Forms.TextBox()
+        Me.button_LastDay = New System.Windows.Forms.Button()
+        Me.lbl_lastDay = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -123,11 +132,92 @@ Partial Class Form1
         Me.RadioButton_DESC.Text = "Descendent"
         Me.RadioButton_DESC.UseVisualStyleBackColor = True
         '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(12, 298)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(191, 20)
+        Me.TextBox9.TabIndex = 11
+        '
+        'button_div
+        '
+        Me.button_div.Location = New System.Drawing.Point(12, 324)
+        Me.button_div.Name = "button_div"
+        Me.button_div.Size = New System.Drawing.Size(190, 23)
+        Me.button_div.TabIndex = 12
+        Me.button_div.Text = "Divisibles"
+        Me.button_div.UseVisualStyleBackColor = True
+        '
+        'lbl_div_numbers
+        '
+        Me.lbl_div_numbers.Location = New System.Drawing.Point(13, 357)
+        Me.lbl_div_numbers.Name = "lbl_div_numbers"
+        Me.lbl_div_numbers.Size = New System.Drawing.Size(190, 16)
+        Me.lbl_div_numbers.TabIndex = 13
+        Me.lbl_div_numbers.Text = "Divisibles: 0"
+        Me.lbl_div_numbers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'TextBox_Month
+        '
+        Me.TextBox_Month.Location = New System.Drawing.Point(252, 38)
+        Me.TextBox_Month.Name = "TextBox_Month"
+        Me.TextBox_Month.Size = New System.Drawing.Size(191, 20)
+        Me.TextBox_Month.TabIndex = 14
+        '
+        'lbl_Month
+        '
+        Me.lbl_Month.Location = New System.Drawing.Point(252, 12)
+        Me.lbl_Month.Name = "lbl_Month"
+        Me.lbl_Month.Size = New System.Drawing.Size(191, 20)
+        Me.lbl_Month.TabIndex = 15
+        Me.lbl_Month.Text = "Mes"
+        '
+        'lbl_year
+        '
+        Me.lbl_year.Location = New System.Drawing.Point(252, 67)
+        Me.lbl_year.Name = "lbl_year"
+        Me.lbl_year.Size = New System.Drawing.Size(191, 20)
+        Me.lbl_year.TabIndex = 17
+        Me.lbl_year.Text = "Year"
+        '
+        'TextBox_Year
+        '
+        Me.TextBox_Year.Location = New System.Drawing.Point(252, 93)
+        Me.TextBox_Year.Name = "TextBox_Year"
+        Me.TextBox_Year.Size = New System.Drawing.Size(191, 20)
+        Me.TextBox_Year.TabIndex = 16
+        '
+        'button_LastDay
+        '
+        Me.button_LastDay.Location = New System.Drawing.Point(252, 119)
+        Me.button_LastDay.Name = "button_LastDay"
+        Me.button_LastDay.Size = New System.Drawing.Size(190, 23)
+        Me.button_LastDay.TabIndex = 18
+        Me.button_LastDay.Text = "Últim dia"
+        Me.button_LastDay.UseVisualStyleBackColor = True
+        '
+        'lbl_lastDay
+        '
+        Me.lbl_lastDay.Location = New System.Drawing.Point(252, 149)
+        Me.lbl_lastDay.Name = "lbl_lastDay"
+        Me.lbl_lastDay.Size = New System.Drawing.Size(191, 23)
+        Me.lbl_lastDay.TabIndex = 19
+        Me.lbl_lastDay.Text = "Últim dia del mes..."
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(215, 352)
+        Me.ClientSize = New System.Drawing.Size(455, 382)
+        Me.Controls.Add(Me.lbl_lastDay)
+        Me.Controls.Add(Me.button_LastDay)
+        Me.Controls.Add(Me.lbl_year)
+        Me.Controls.Add(Me.TextBox_Year)
+        Me.Controls.Add(Me.lbl_Month)
+        Me.Controls.Add(Me.TextBox_Month)
+        Me.Controls.Add(Me.lbl_div_numbers)
+        Me.Controls.Add(Me.button_div)
+        Me.Controls.Add(Me.TextBox9)
         Me.Controls.Add(Me.RadioButton_DESC)
         Me.Controls.Add(Me.RadioButton_ASC)
         Me.Controls.Add(Me.button_order)
@@ -157,4 +247,13 @@ Partial Class Form1
     Friend WithEvents button_order As Button
     Friend WithEvents RadioButton_ASC As RadioButton
     Friend WithEvents RadioButton_DESC As RadioButton
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents button_div As Button
+    Friend WithEvents lbl_div_numbers As Label
+    Friend WithEvents TextBox_Month As TextBox
+    Friend WithEvents lbl_Month As Label
+    Friend WithEvents lbl_year As Label
+    Friend WithEvents TextBox_Year As TextBox
+    Friend WithEvents button_LastDay As Button
+    Friend WithEvents lbl_lastDay As Label
 End Class
--- vb-project02/Form1.vb
+++ vb-project02/Form1.vb
@@ -10,12 +10,12 @@
 
         Dim orderedNumbers(8) As Double
         Dim canOrder As Boolean = True
-        Dim counter As Int64 = 0
+        Dim counter As Integer = 0
 
         For index As Integer = 0 To 7
 
             Dim textBox As TextBox = textBoxs(index)
-            Dim number As Int64
+            Dim number As Integer
 
             textBox.BackColor = Color.White
 
@@ -69,16 +69,106 @@
         End If
     End Sub
 
-    Private Function getParsedDouble(textbox As TextBox)
-        If textbox.Text.Length = 0 Then
-            Return Nothing
+
+    Private Sub button_div_MouseUp(sender As Object, e As MouseEventArgs) Handles button_div.MouseUp
+        textBoxs = {
+            TextBox1, TextBox2, TextBox3, TextBox4,
+            TextBox5, TextBox6, TextBox7, TextBox8,
+            TextBox9
+        }
+
+        Dim orderedNumbers(8) As Double
+        Dim canOrder As Boolean = True
+        Dim counter As Integer = 0
+
+        For index As Integer = 0 To 7
+
+            Dim textBox As TextBox = textBoxs(index)
+            Dim number As Integer
+
+            textBox.BackColor = Color.White
+
+            If Double.TryParse(textBox.Text, number) Then
+
+                orderedNumbers(counter) = number
+                counter += 1
+
+            Else
+                If textBox.Text IsNot "" Then
+
+                    textBox.Focus()
+                    textBox.BackColor = Color.Red
+                    canOrder = False
+                    Exit For
+                End If
+            End If
+        Next
+        counter -= 1
+
+        Dim number9 As Integer
+        textBoxs(8).BackColor = Color.White
+        If Not Double.TryParse(textBoxs(8).Text, number9) Then
+
+            textBoxs(8).Focus()
+            textBoxs(8).BackColor = Color.Red
+            canOrder = False
         End If
-        Dim number As Double
-        If Double.TryParse(textbox.Text, number) Then
-            Return number
+
+        If canOrder Then
+
+            Dim div_numbers As Integer = 0
+            Dim newNumbers(counter) As Double
+            For index As Integer = 0 To counter
+                newNumbers(index) = orderedNumbers(index)
+            Next
+
+            For index As Integer = 0 To counter
+                Dim textBox As TextBox = textBoxs(index)
+                If (newNumbers(index) / number9) = Int(newNumbers(index) / number9) Then
+                    div_numbers += 1
+                End If
+            Next
+
+            lbl_div_numbers.Text = "Divisibles:" + CStr(div_numbers)
+
+        End If
+
+    End Sub
+
+    Private Sub button_LastDay_MouseUp(sender As Object, e As MouseEventArgs) Handles button_LastDay.MouseUp
+
+        Dim year As Integer
+        Dim month As Integer
+
+        TextBox_Month.BackColor = Color.White
+        TextBox_Year.BackColor = Color.White
+
+        If Double.TryParse(TextBox_Month.Text, month) Then
+            If month <= 0 Or month > 12 Then
+                TextBox_Month.Focus()
+                TextBox_Month.BackColor = Color.Red
+                Exit Sub
+            End If
         Else
-            textbox.Focus()
-            Return Nothing
+            TextBox_Month.Focus()
+            TextBox_Month.BackColor = Color.Red
+            Exit Sub
         End If
-    End Function
+
+        If Double.TryParse(TextBox_Year.Text, year) Then
+            If year <= 0 Then
+                TextBox_Year.Focus()
+                TextBox_Year.BackColor = Color.Red
+                Exit Sub
+            End If
+        Else
+
+            TextBox_Year.Focus()
+            TextBox_Year.BackColor = Color.Red
+            Exit Sub
+        End If
+
+        lbl_lastDay.Text = "Últim dia del mes..." + CStr(System.DateTime.DaysInMonth(year, month))
+
+    End Sub
 End Class
